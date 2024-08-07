71c37b75141c6a2b3427f81ee0bb6aeba8d964ec
Andrés Felipe Castillo Jiménez <69415650+Andresito25@users.noreply.github.com>
2024-08-07 04:03:28 -0500

    Update cocktails.blade.php

--- resources/views/cocktails.blade.php
+++ resources/views/cocktails.blade.php
@@ -7,28 +7,7 @@
 
           <div
             class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 text-center transition ease-in-out delay-150">
-            {{-- @foreach ($cocktails as $cocktail)
-              <div
-                class="flex flex-col gap-5 border-2 border-green-600 rounded-lg h-max p-7 hover:shadow-lg hover:shadow-green-600 hover:translate-y-1 transition ease-in-out delay-150">
-                <h1 class="text-wrap text-3xl">{{ $cocktail['name'] }}</h1>
-
-                <img class="rounded-lg" src="{{ $cocktail['image'] }}" alt="cocktail">
-
-                <p class="text-sm overflow-hidden truncate hover:text-balance transition ease-in-out delay-1000">
-                  {{ $cocktail['preparation'] }}</p>
-
-                <button type="button"
-                  class="border-2 border-purple-700 p-3 rounded-lg text-slate-400 text-base  hover:bg-purple-500 hover:shadow-md hover:shadow-purple-500 hover:text-white">
-                  Guardar
-                </button>
-
-                <button type="button"
-                  class="border-2 border-green-700 p-3 rounded-lg text-slate-400 text-base  hover:bg-green-500 hover:shadow-md hover:shadow-green-500 hover:text-black">
-                  Ver cóctel
-                </button>
-              </div>
-            @endforeach --}}
-            Vista del cóctel
+            Vista del cóctel por medio del id
           </div>
 
         </div>
